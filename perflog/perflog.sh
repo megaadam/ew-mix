d7fb3bdd79eca19a3fa2bd2c80867c54ea84fe43
adam <adam.horvath@edgeware.tv>
2020-10-14 10:08:42 +0200

    add perflog

---
+++ perflog/perflog.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+samplerate=10
+sepflag=0
+
+while [ 1 ]; do
+
+    j8count=$(/bin/ps -elf | grep make |  grep  '\-j 8'  | wc -l)
+    mpstat=$(mpstat -P all $samplerate 1 -o JSON)
+    ps2=$(ps -e -o pcpu,comm,args --sort=-pcpu)
+    #for entry in $ps2; do
+        # echo ... $entry
+    #done
+    ps=$( ps -e -o pcpu,comm --sort=-pcpu | \
+          awk  '{ORS=","; if ($1 > 9) print  "{cpu: " $1 ", proc: " $2 "}" }' | \
+          sed 's/.$//' )
+
+    if [ $sepflag = 1 ]; then
+       echo ","
+    fi
+    sepflag=1
+    echo {
+
+    echo '    "date":      "'$(date)'"',
+    echo '    "j8_count":  "'$j8count'"',
+    echo '    "ps":        "['$ps']"',
+    echo '    "mpstat":    '$mpstat
+    echo }
+    echo
+done
+
